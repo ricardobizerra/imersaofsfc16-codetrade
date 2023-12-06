41575d6d7fb378662aec9db02fabe91fa5218840
ricardobizerra <ricardo.bizerra19@gmail.com>
2023-12-06 11:09:43 -0300

    feat: book entity and methods

--- internal/market/entity/transaction.go
+++ internal/market/entity/transaction.go
@@ -29,3 +29,27 @@ func NewTransaction(sellingOrder *Order, buyingOrder *Order, shares int, price f
 		DateTime: time.Now(),
 	}
 }
+
+func (t *Transaction) CalculateTotal(shares int, price float64) {
+	t.Total = float64(t.Shares) * t.Price
+}
+
+func (t *Transaction) CloseBuyOrder() {
+	if t.BuyingOrder.PendingShares == 0 {
+		t.BuyingOrder.Status = "CLOSED"
+	}
+}
+
+func (t *Transaction) AddBuyOrderPendingShares(shares int) {
+	t.BuyingOrder.PendingShares += shares
+}
+
+func (t *Transaction) CloseSellOrder() {
+	if t.SellingOrder.PendingShares == 0 {
+		t.SellingOrder.Status = "CLOSED"
+	}
+}
+
+func (t *Transaction) AddSellOrderPendingShares(shares int) {
+	t.SellingOrder.PendingShares += shares
+}
