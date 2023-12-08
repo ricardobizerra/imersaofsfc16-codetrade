2ee5c0a463826d07617cd951c8454d8d68e1043c
ricardobizerra <ricardo.bizerra19@gmail.com>
2023-12-08 00:00:28 -0300

    chore: docker setup

--- go/cmd/trade/main.go
+++ go/cmd/trade/main.go
@@ -37,6 +37,7 @@ func main() {
 	go func() {
 		for msg := range kafkaMsgChan {
 			wg.Add(1) // "wg" is a WaitGroup, used to wait for all goroutines to finish
+			fmt.Println(string(msg.Value))
 
 			tradeInput := dto.TradeInput{}
 			err := json.Unmarshal(msg.Value, &tradeInput)
