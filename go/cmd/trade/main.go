5224fec2b8a071fc482bf9760290ba5b735faf0f
ricardobizerra <ricardo.bizerra19@gmail.com>
2023-12-07 23:41:23 -0300

    feat: main trading function

---
+++ go/cmd/trade/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"sync"
+
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/devfullcycle/imersao13/go/internal/infra/kafka"
+	"github.com/devfullcycle/imersao13/go/internal/market/dto"
+	"github.com/devfullcycle/imersao13/go/internal/market/entity"
+	"github.com/devfullcycle/imersao13/go/internal/market/transformer"
+)
+
+func main() {
+	ordersIn := make(chan *entity.Order)
+	ordersOut := make(chan *entity.Order)
+
+	wg := &sync.WaitGroup{}
+	defer wg.Wait()
+
+	kafkaMsgChan := make(chan *ckafka.Message)
+	configMap := &ckafka.ConfigMap{
+		"bootstrap.servers": "host.docker.internal:9094",
+		"group.id":          "rbGroup",
+		"auto.offset.reset": "earliest",
+	}
+
+	producer := kafka.NewKafkaProducer(configMap)
+	kafka := kafka.NewConsumer(configMap, []string{"input"})
+
+	go kafka.Consume(kafkaMsgChan) // "go" alocates a new thread to run this function
+
+	book := entity.NewBook(ordersIn, ordersOut, wg)
+	go book.Trade()
+
+	go func() {
+		for msg := range kafkaMsgChan {
+			wg.Add(1) // "wg" is a WaitGroup, used to wait for all goroutines to finish
+
+			tradeInput := dto.TradeInput{}
+			err := json.Unmarshal(msg.Value, &tradeInput)
+			if err != nil {
+				panic(err)
+			}
+
+			order := transformer.TrasformInput(tradeInput)
+			ordersIn <- order
+		}
+	}()
+
+	for res := range ordersOut {
+		output := transformer.TransformOutput(res)
+		outputJson, err := json.MarshalIndent(output, "", "  ")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		producer.Publish(outputJson, []byte("orders"), "output")
+	}
+}
